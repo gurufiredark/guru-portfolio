11ab2d414fb370deb72571c895962ee541a91af1
Gabriel <gurufiredark@gmail.com>
2024-12-10 18:45:16 -0300

    Atualização da configuração para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/guru-portfolio',
+  assetPrefix: '/guru-portfolio',
 };
 
 export default nextConfig;
